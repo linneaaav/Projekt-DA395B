ccc6771667ad9536aa3099c9171fba08e7d073cc
Linnea Värnström <linnea.varnstrom@outlook.com>
2026-05-26 01:34:19 +0200

    Fetch country and flag from REST Countries API

--- flag-quiz/src/App.jsx
+++ flag-quiz/src/App.jsx
@@ -1,10 +1,12 @@
 /* Här kommer själva Flagg-Quizen ligga */
-
+import FlagQuizApplication from './components/FlagQuizApplication/FlagQuizApplication';
 import './app.css';
 
 const App = () => {
   return(
-    <></>
+    <>
+      <FlagQuizApplication />
+    </>
   )
 }
 
