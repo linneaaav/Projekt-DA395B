834f7b315e22b64926330071353ab6692e8df69d
Linnea Värnström <linnea.varnstrom@outlook.com>
2026-05-25 19:47:43 +0200

    Add FlagQuizApplication component

---
+++ flag-quiz/src/components/flagQuizApplication/FlagQuizApplication.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+const FlagQuizApplication = () => {
+    return (
+        <div className="flex items-center">
+            // Lägg till komponenter
+        </div>
+    );
+};
+
+export default FlagQuizApplication;
