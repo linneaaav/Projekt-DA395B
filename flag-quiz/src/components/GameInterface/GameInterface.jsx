e6b86f60c3270f321934e587ff8042f9ad1beeed
cs <eseoics@gmail.com>
2026-05-31 18:45:12 +0200

    fix: colors

--- flag-quiz/src/components/GameInterface/GameInterface.jsx
+++ flag-quiz/src/components/GameInterface/GameInterface.jsx
@@ -78,7 +78,7 @@ const GameInterface = ({ gameCountry, onCorrectGuess, onGameEnd }) => {
 
             <section className="w-full max-w-lg rounded-xl border border-blue-500 bg-gray-800 p-4 m-4">
                 <p className="text-sm uppercase tracking-wide">
-                    <span className="text-gray">Points: </span>
+                    <span className="text-lightgray-600">Points: </span>
                     <span className="text-green-600 font-bold">{results.length}</span>
                 </p>
             </section>
